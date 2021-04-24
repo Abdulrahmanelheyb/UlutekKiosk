d7f2d351ef3c42e7e762e6e8e1dfc9c81da17e78
Abdulrahman Elheyb <45883300+Abdulrahmanelheyb@users.noreply.github.com>
2021-04-24 17:24:19 +0300

    project init

---
+++ UlutekKiosk/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using UlutekKiosk.BLL;
+using UlutekKiosk.OAL;
+
+/*
+ * 
+ > Creation Date: 3/29/2021
+ > Copyright: (C) Ulutek
+ > Developed By Abdulrahman Elheyb
+ > Author: Ulutek
+
+ */
+
+namespace UlutekKiosk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Publication> pubs;
+        Duration duration;
+        DoubleAnimation doubleAnimation;
+
+        int pubIndx = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pubs = PublicationServices.GetPublications();
+            if (pubs.Count > 0)
+            {
+                SlaytShow.Source = pubs[0].Image;
+                InitProgressAnimation(pubs[0].TimeOfView);
+                pubIndx++;
+            }
+            
+        }
+
+        private void DoubleAnimation_Completed(object sender, EventArgs e)
+        {
+            if (pubIndx == pubs.Count)
+            {
+                pubIndx = 0;
+            }
+
+            SlaytShow.Dispatcher.Invoke(() => { SlaytShow.Source = pubs[pubIndx].Image; });
+            InitProgressAnimation(pubs[pubIndx].TimeOfView);
+            pubIndx++;
+        }
+
+        private void InitProgressAnimation(int TimeOfView)
+        {
+            duration = new Duration(TimeSpan.FromSeconds(TimeOfView));
+            doubleAnimation = new DoubleAnimation(100, duration);
+            doubleAnimation.Completed += DoubleAnimation_Completed;
+            doubleAnimation.FillBehavior = FillBehavior.Stop;
+            SlaytShowProgress.BeginAnimation(ProgressBar.ValueProperty, doubleAnimation, HandoffBehavior.SnapshotAndReplace);
+        }
+    }
+}
---
+++ UlutekKiosk/Mysqldb.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Net.NetworkInformation;
+using MySql.Data.MySqlClient;
+
+namespace SBSS_Models
+{
+    public class Mysqldb
+    {
+        // This mysqldb class is contain all mysql services.
+
+        public static string ServerIP = Properties.Settings.Default.ServerIP; //"192.168.0.134";
+        private static string Database = "sbss";
+        private static string Uid = "schoolserver";
+        private static string Pwd = "School+admin3";
+        public static ConnectionState State { get { return Connection.State; } }
+
+        private static MySqlConnection Connection = new MySqlConnection($"Server={ServerIP};Database={Database};Uid={Uid};Pwd={Pwd}; Connect Timeout=1");
+
+        public static void OpenConnection()
+        {
+            try
+            {
+                Connection.OpenAsync();
+            }
+            catch (Exception) { }
+        }
+
+        public static void Close()
+        {
+            Connection.Close();
+        }
+
+        public static DataSet Select(string query)
+        {
+            // Read data from mysql server
+            DataSet dt = new DataSet();
+            try
+            {
+                if (NetworkInterface.GetIsNetworkAvailable() && Connection.State == ConnectionState.Open)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, Connection)
+                    {
+                        CommandTimeout = 0
+                    };
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    da.Fill(dt);
+                }
+            }
+            catch (Exception ex) { throw ex; }
+            return dt;
+        }
+
+        public static void Update(string query)
+        {
+            try
+            {
+                if (NetworkInterface.GetIsNetworkAvailable() && Connection.State == ConnectionState.Open)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, Connection)
+                    {
+                        CommandTimeout = 0
+                    };
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex) { throw ex; }
+        }
+    }
+}
